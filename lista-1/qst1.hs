324f0f1f07b3e77f5d3e1dfefff70b455763794f
Bruno Marchi Pires <45456467+Brunokrk@users.noreply.github.com>
2024-03-08 12:12:04 -0300

    questão 1 feita

--- lista-1/qst1.hs
+++ lista-1/qst1.hs
@@ -6,5 +6,46 @@ ins e n@(Node x l r)
     | e < x = Node x (ins e l) r
     | e > x = Node x l (ins e r)
 
+remove :: (Ord a) => a -> Tree a -> Tree a
+remove _ Leaf = Leaf  --elemento não encontrado
+remove e (Node x l r)
+  | e < x = Node x (remove e l) r  --subárvore esquerda
+  | e > x = Node x l (remove e r)  --ubárvore direita
+  | otherwise = removeNode (Node x l r)  --Nó encontrado
 
-    
+removeNode :: (Ord a) => Tree a -> Tree a
+removeNode (Node _ Leaf Leaf) = Leaf  --é uma folha
+removeNode (Node _ l Leaf) = l  -- somente filho a esquerda
+removeNode (Node _ Leaf r) = r  -- somente filho a direita
+removeNode (Node _ l r) =  -- tem dois filhos... F
+  let (minNode, newRight) = extractMin r
+  in Node minNode l newRight
+
+-- Função auxiliar para extrair o menor nó da subárvore direita e retorná-lo
+-- junto com a nova subárvore direita sem o menor nó.
+extractMin :: (Ord a) => Tree a -> (a, Tree a)
+extractMin (Node x Leaf r) = (x, r)
+extractMin (Node x l r) =
+  let (minNode, newLeft) = extractMin l
+  in (minNode, Node x newLeft r)
+
+printTree :: (Show a) => Tree a -> IO ()
+printTree tree = putStr (treeToString 0 tree)
+  where
+    treeToString :: (Show a) => Int -> Tree a -> String
+    treeToString _ Leaf = ""
+    treeToString depth (Node x left right) =
+      treeToString (depth + 1) right ++
+      replicate (depth * 4) ' ' ++ show x ++ "\n" ++
+      treeToString (depth + 1) left
+
+main :: IO ()
+main = do
+    let tree = foldr ins Leaf [8, 6, 4, 2, 7, 3, 5]
+    putStrLn "Árvore original:"
+    --print tree
+    printTree tree
+    let treeAfterRemoval = remove 3 tree
+    putStrLn "Árvore após a remoção do elemento 3:"
+    --print treeAfterRemoval
+    printTree treeAfterRemoval
